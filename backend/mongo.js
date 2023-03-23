ce0e7f40c887ebc1bf389e95481ff5508cd9b047
d-rocco <dhrocco27@gmail.com>
2023-03-23 13:32:39 -0400

    Complete exercise 3.12

---
+++ backend/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 5 && process.argv.length > 3) {
+  console.log("give all arguments!");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://dhrocco:${password}@cluster.i0ki2pf.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name,
+  number,
+});
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((note) => {
+      console.log(`${note.name} ${note.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
