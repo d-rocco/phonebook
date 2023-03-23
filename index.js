28f8bc37030cbb2254ebde41e9c35d9dcb3c9769
d-rocco <dhrocco27@gmail.com>
2023-03-22 20:48:09 -0400

    Complete exercise 3.4

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ app.get("/info", (request, response) => {
   );
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => id !== person.id);
+
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
